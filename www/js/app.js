49da29899860513f25a167b5f08b9a756857349e
ccoenraets <ccoenraets@gmail.com>
2014-09-04 10:23:17 -0400

    Updated for Mobile SDK 2.3 GA

--- www/js/app.js
+++ www/js/app.js
@@ -1,48 +1,44 @@
-(function () {
+function authenticate(successHandler, errorHandler) {
 
-    "use strict";
+    // Get reference to Salesforce OAuth plugin
+    var oauthPlugin = cordova.require("com.salesforce.plugin.oauth");
 
-    function authenticate(successHandler, errorHandler) {
+    // Authenticate
+    oauthPlugin.getAuthCredentials(
+        function (creds) {
+            console.log(JSON.stringify(creds));
+            // Initialize ForceJS
+            force.init({accessToken: creds.accessToken, instanceURL: creds.instanceUrl, refreshToken: creds.refreshToken});
+            if (successHandler) successHandler();
+        },
+        function (error) {
+            console.log(error);
+            if (errorHandler) errorHandler(error);
+        }
+    );
+}
 
-        // Get reference to Salesforce OAuth plugin
-        var oauthPlugin = cordova.require("com.salesforce.plugin.oauth");
-
-        // Authenticate
-        oauthPlugin.getAuthCredentials(
-            function (creds) {
-                // Instantiate ForceTK client
-                var forceClient = new forcetk.Client();
-                forceClient.setSessionToken(creds.accessToken, "v31.0", creds.instanceUrl);
-                if (successHandler) successHandler(forceClient);
-            },
-            function (error) {
-                if (errorHandler) errorHandler(error);
+function showContacts() {
+    force.query('SELECT Name, Phone FROM Contact LIMIT 20',
+        function(data) {
+            var contacts = data.records,
+                html = '';
+            for (var i=0; i < contacts.length; i++) {
+                html += ('<li class="table-view-cell"><div class="media-body">' + contacts[i].Name + '<p>' + contacts[i].Phone + '</p></div></li>');
             }
-        );
-    }
+            document.querySelector('#contacts').innerHTML = html;
+        },
+        function(error) {
+            alert("Can't load contacts: " + error);
+        });
 
-    function showContacts(forceClient) {
+}
 
-        forceClient.query('SELECT Name, Phone FROM Contact LIMIT 20',
-            function(data) {
-                var contacts = data.records,
-                    html = '';
-                for (var i=0; i < contacts.length; i++) {
-                    html += ('<li class="table-view-cell"><div class="media-body">' + contacts[i].Name + '<p>' + contacts[i].Phone + '</p></div></li>');
-                }
-                document.querySelector('#contacts').innerHTML = html;
-            },
-            function(error) {
-                alert("Can't load contacts: " + error);
-            });
-
-    }
-
-    // Wait for Cordova to be fully initialized
-    document.addEventListener("deviceready", function() {
+// Wait for Cordova to be fully initialized
+document.addEventListener("deviceready",
+    function() {
         authenticate(showContacts, function(error) {
             alert('Authentication failed: ' + error);
         });
-    }, false);
-
-})();
+    },
+    false);
